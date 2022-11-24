9b85608d503aaec7dc88a87b558f5df3b8daf945
donnaolidana <115701841+donnaolidana@users.noreply.github.com>
2022-11-24 20:10:24 +0800

    added leaderboard and grid container (#1)

---
+++ react-login-register-ui/src/components/LeaderboardItem.js
@@ -0,0 +1,22 @@
+import ListGroup from "react-bootstrap/ListGroup";
+import Badge from 'react-bootstrap/Badge';
+
+const LeaderboardItem = (item) => {
+    const details = item.leader
+    return (
+        <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start"
+        >
+            <div className="ms-2 me-auto">
+                <div className="fw-bold">{details.name}</div>
+            </div>
+            <Badge bg="primary" pill>
+                {details.streak}
+            </Badge>
+        </ListGroup.Item>
+    )
+
+}
+
+export default LeaderboardItem
