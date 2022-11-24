e4bf22b8457664489911aae55604f588ff8c91b9
pangace@MFCGD.com <celeste_june_panganiban@manulife.com>
2022-11-24 11:17:17 +0800

    with login & signup

---
+++ react-login-register-ui/src/components/Login.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+    render() {
+        return (
+            // <div class="align-right">
+                <form>
+
+                <h3>Log in</h3>
+
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Enter Email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group mt-2">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-center mt-3">
+                    <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+                </div>
+
+                <p className="forgot-password text-center">
+                    Forgot <a href="#">password?</a>
+                </p>
+                </form>
+            // </div>
+            
+        );
+    }
+}
---
+++ react-login-register-ui/src/components/Signup.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Register</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div class="d-flex justify-content-center mt-3">
+                    <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+                </div>
+                
+                <p className="forgot-password text-center">
+                    Already registered <a href="#">log in?</a>
+                </p>
+            </form>
+        );
+    }
+}
